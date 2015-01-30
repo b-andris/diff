414092fdadd04a7ad4e5e5056184ae6f1375e229
Benjamin Andris Suter-Dörig <benjaminandris@yahoo.de>
2015-01-30 16:19:14 +0100

    Initial Commit

---
+++ diff/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  diff
+//
+//  Created by Benjamin Andris Suter-Dörig on 30/01/15.
+//  Copyright (c) 2015 Benjamin Andris Suter-Dörig. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
